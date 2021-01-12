276d40bc88061f08d58fe46408ab0056b9cb4557
DN19-001\s.okada <s.okada@wisdom.co.jp>
2021-01-12 17:27:47 +0900

    コントローラの作成

---
+++ src/main/java/jp/co/wisdom/newcommer2020/controller/SampleController.java
@@ -0,0 +1,23 @@
+package jp.co.wisdom.newcommer2020.controller;
+
+/**
+ * @author s.okada
+ * Spring管理のクラス
+ */
+
+public class SampleController {
+
+	/**
+	 * getリクエストのときに動作するメソッド
+	 */
+	public void getProc() {
+
+	}
+
+	/**
+	 * postリクエストのときに動作するメソッド
+	 */
+	public void postProc() {
+
+	}
+}
---
+++ src/main/java/jp/co/wisdom/newcommer2020/service/sampleest/SampleRestService.java
@@ -0,0 +1,10 @@
+package jp.co.wisdom.newcommer2020.service.sampleest;
+
+/**
+ * @author s.okada
+ * Spring管理のクラス
+ */
+public class SampleRestService {
+
+
+}
