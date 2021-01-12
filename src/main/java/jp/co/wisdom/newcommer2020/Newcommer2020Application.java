a238ead1cdd8a6dfbc378a54265136a29edb585a
DN20-001\kiichi <kiichi@wisdom.co.jp>
2021-01-12 10:29:38 +0900

    initial commit

---
+++ src/main/java/jp/co/wisdom/newcommer2020/Newcommer2020Application.java
@@ -0,0 +1,13 @@
+package jp.co.wisdom.newcommer2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Newcommer2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Newcommer2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/wisdom/newcommer2020/Newcommer2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.wisdom.newcommer2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Newcommer2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
