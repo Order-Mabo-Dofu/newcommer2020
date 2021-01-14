c1116d7148f6e19c83f8a6e96de3ccd4a9a83b1f
DN19-001\s.okada <s.okada@wisdom.co.jp>
2021-01-14 11:08:57 +0900

    お題に合うように修正(2) @RestControllerが抜けていた大問題も修正

--- src/main/java/jp/co/wisdom/newcommer2020/controller/SampleController.java
+++ src/main/java/jp/co/wisdom/newcommer2020/controller/SampleController.java
@@ -1,7 +1,11 @@
 package jp.co.wisdom.newcommer2020.controller;
 
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import jp.co.wisdom.newcommer2020.controller.bean.Greeting;
 
@@ -9,16 +13,17 @@ import jp.co.wisdom.newcommer2020.controller.bean.Greeting;
  * @author s.okada
  * Spring管理のクラス
  */
+@RestController
 public class SampleController {
 
 	/**
 	 * あいさつ文
 	 */
-	private static final String template = "Hello, World!";
+	private final String template = "Hello, World!";
 	/**
 	 * 識別子
 	 */
-	private final int num = 9;
+	private AtomicLong counter = new AtomicLong();
 
 	/**
 	 * getリクエストのときに動作するメソッド
@@ -26,8 +31,8 @@ public class SampleController {
 	 * @return リソース表現
 	 */
 	@GetMapping("/greeting")
-	public Greeting getProc() {
-		return new Greeting(num,template);
+	public Greeting getProc(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
 	/**
