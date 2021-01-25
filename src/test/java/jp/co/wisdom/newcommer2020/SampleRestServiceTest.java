21ef9f7295762378cdce7893ad2b5efc78bb92f1
DN19-001\s.okada <s.okada@wisdom.co.jp>
2021-01-25 16:20:54 +0900

    テストメソッドの統合

--- src/test/java/jp/co/wisdom/newcommer2020/SampleRestServiceTest.java
+++ src/test/java/jp/co/wisdom/newcommer2020/SampleRestServiceTest.java
@@ -25,80 +25,54 @@ public class SampleRestServiceTest {
 	private SampleRestService sampleRestService;
 
 	/**
-	 * 時間帯に応じたあいさつ文作成テスト（5時～11時）
-	 * processのテストメソッドで１つに統合予定。
+	 * 時間帯に応じたあいさつ文作成テスト
 	 */
 	@Test
-    public void processTest1() {
-		//LocalDateTimeに11時59分59秒をセット（セット時間 < 12時）
-    	LocalDateTime lt = LocalDateTime.of(9999, 12, 31, 11, 59, 59, 99);
+    public void processTest() {
 
-        // 対象クラスのモック化
-        MockedStatic<LocalDateTime> mocked = Mockito.mockStatic(LocalDateTime.class);
-        // 戻り値を設定してスタブ化
-        mocked.when(LocalDateTime::now).thenReturn(lt);
-
-        //入力値（画面から受け取るvalue）をセット
+		//入力値（全時間帯共通の画面から受け取るvalue）をセット
 		String name = "World";
-		//期待値（時間帯に応じたあいさつ文）
-		String expContent = "Good morning,World!";
 
-		//テストクラスの実行
-		Greeting greTest = sampleRestService.process(name);
-		//返り値と期待値が合っているかの確認
-		assertThat(greTest.getContent()).isEqualTo(expContent);
-
-		//テスト終了後にMockを閉じる
-		mocked.close();
-	}
-
-	/**
-	 * 時間帯に応じたあいさつ文作成テスト（12時～18時）
-	 */
-	@Test
-    public void processTest2() {
-		//LocalDateTimeに18時59分59秒をセット（セット時間 < 19時）
-		LocalDateTime lt = LocalDateTime.of(9999, 12, 31, 18, 59, 59, 99);
-
-        // 対象クラスのモック化
-        MockedStatic<LocalDateTime> mocked = Mockito.mockStatic(LocalDateTime.class);
-
-        // 戻り値を設定してスタブ化
-        mocked.when(LocalDateTime::now).thenReturn(lt);
-
-        String name = "World";
-
-		String expContent = "Hello,World!";
-
-		Greeting greTest = sampleRestService.process(name);
-
-		assertThat(greTest.getContent()).isEqualTo(expContent);
-
-        mocked.close();
-	}
-	/**
-	 * 時間帯に応じたあいさつ文作成テスト（19時～4時）
-	 *
-	 */
-	@Test
-    public void processTest3() {
-		//LocalDateTimeに3時59分59秒をセット（セット時間 < 4時）
+		//LocalDateTimeに3時59分59秒をセット（0時 <= セット時間 < 4時）
 		LocalDateTime lt = LocalDateTime.of(9999, 12, 31, 3, 59, 59, 99);
-
         // 対象クラスのモック化
         MockedStatic<LocalDateTime> mocked = Mockito.mockStatic(LocalDateTime.class);
-
         // 戻り値を設定してスタブ化
         mocked.when(LocalDateTime::now).thenReturn(lt);
+        //テストメソッドの実行(0時～3時にアクセスした場合)
+		Greeting greTestE = sampleRestService.process(name);
 
-        String name = "World";
+		//LocalDateTimeに時間を加算して11時59分59秒をセット（4時 <= セット時間 < 12時）
+		lt = lt.plusHours(8);
+		// 戻り値の時間を更新
+		mocked.when(LocalDateTime::now).thenReturn(lt);
+		//テストメソッドの実行(5時～11時にアクセスした場合)
+		Greeting greTestM = sampleRestService.process(name);
 
-		String expContent = "Good evening,World!";
+		//LocalDateTimeに時間を加算して18時59分59秒をセット（12時 <= セット時間 < 19時）
+		lt = lt.plusHours(7);
+		// 戻り値の時間を更新
+		mocked.when(LocalDateTime::now).thenReturn(lt);
+		//テストメソッドの実行(12時～18時にアクセスした場合)
+		Greeting greTestH = sampleRestService.process(name);
 
-		Greeting greTest = sampleRestService.process(name);
+		//LocalDateTimeに時間を加算して23時59分59秒をセット（19時 <= セット時間 < 24時）
+		lt = lt.plusHours(5);
+		// 戻り値の時間を更新
+		mocked.when(LocalDateTime::now).thenReturn(lt);
+		//テストメソッドの実行(19時～24時にアクセスした場合)
+		Greeting greTestE2 = sampleRestService.process(name);
 
-		assertThat(greTest.getContent()).isEqualTo(expContent);
+		//期待値（作成される時間帯に応じたあいさつ文）をセット
+		String expE = "Good evening,World!";
+		String expM = "Good morning,World!";
+		String expH = "Hello,World!";
+		//返り値と期待値が合っているかの確認
+		assertThat(greTestE.getContent()).isEqualTo(expE);
+		assertThat(greTestM.getContent()).isEqualTo(expM);
+		assertThat(greTestH.getContent()).isEqualTo(expH);
+		assertThat(greTestE2.getContent()).isEqualTo(expE);
 
-        mocked.close();
+		mocked.close();
 	}
 }
